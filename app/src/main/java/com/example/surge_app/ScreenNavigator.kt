5b6c2bc4ecfaff7b46292cb23a54ec9217197172
RedWyrm13 <Andrewmaciejunes@yahoo.com>
2023-11-14 22:23:07 -0500

    Created NavHost

--- app/src/main/java/com/example/surge_app/ScreenNavigator.kt
+++ app/src/main/java/com/example/surge_app/ScreenNavigator.kt
@@ -1,11 +1,21 @@
 package com.example.surge_app
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.composable
+import androidx.compose.foundation.layout.padding
+import androidx.navigation.NavHostController
 
-enum class(){
+
+enum class Screens(){
     loginOrCreateAccount,
     createAccount
 }
 
 @Composable
-fun SurgeApp
+fun SurgeApp(navController: NavHostController = rememberNavController(){
+    NavHost(navController = navController){
+
+    }
+}
