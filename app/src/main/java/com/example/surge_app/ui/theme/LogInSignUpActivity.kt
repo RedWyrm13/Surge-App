a082058e54ba6314808d248342e5ad03031c82c0
RedWyrm13 <Andrewmaciejunes@yahoo.com>
2023-11-17 02:42:53 -0500

    Created LogInSignUpActivity.kt

---
+++ app/src/main/java/com/example/surge_app/ui/theme/LogInSignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.surge_app.ui.theme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.surge_app.R
+
+class LogInSignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in_sign_up)
+    }
+}
