58f6a1973c0d460392ded792bacc83a9d0e3b77f
RedWyrm13 <Andrewmaciejunes@yahoo.com>
2024-03-12 22:35:28 -0400

    routesPostRequest returned example properly. Need to add user input into post request.

--- app/src/main/java/com/example/surge_app/data/ApiKey.kt
+++ app/src/main/java/com/example/surge_app/data/ApiKey.kt
@@ -4,4 +4,6 @@ package com.example.surge_app.data
 //We need to make this more secure. I am unsure how atm
 object ApiKey {
     const val apiKey ="AIzaSyDmcIQskDfuV7-mn33TTHCX8H635UNifGI"
+    const val accessToken = "ya29.a0Ad52N39Pk9RCbFFDEAyp1zCA7nhUgEyhOEXglB6JZ53pRsfFzvMmE95ZsvZuDDDVIppaF5BedSwLvmBUsPEp0w6sJ9NLu_Z4pIyZ08qS8AjQLuWFufAWLLsHjkAjs_P44PiwkcL3IGkwmKdZfm7BfcKcyMfVCc4-bwWxaCgYKAaQSARASFQHGX2Mim7IY-_Ktk5WR131pZncbZw0171"
+    const val projectId = "surge-f7068 "
 }
--- app/src/main/java/com/example/surge_app/network/RoutesApiService.kt
+++ app/src/main/java/com/example/surge_app/network/RoutesApiService.kt
@@ -4,72 +4,82 @@ import android.util.Log
 import com.example.surge_app.data.ApiKey
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 import java.net.HttpURLConnection
 import java.net.URL
-
 suspend fun routesPostRequest(): String {
     return withContext(Dispatchers.IO) {
-        Log.d("Response", "routesPostRequest is running")
-        val apiKey = ApiKey.apiKey
+        Log.d("MyTag", "Coroutine starting!")
+
+        val apiKey = "YOUR_API_KEY"
         val urlString = "https://routes.googleapis.com/directions/v2:computeRoutes"
         val jsonData = """
-        {
-          "origin":{
-            "location":{
-              "latLng":{
-                "latitude": 37.419734,
-                "longitude": -122.0827784
-              }
+            {
+              "origin":{
+                "location":{
+                  "latLng":{
+                    "latitude": 37.419734,
+                    "longitude": -122.0827784
+                  }
+                }
+              },
+              "destination":{
+                "location":{
+                  "latLng":{
+                    "latitude": 37.417670,
+                    "longitude": -122.079595
+                  }
+                }
+              },
+              "travelMode": "DRIVE",
+              "routingPreference": "TRAFFIC_AWARE",
+              "departureTime": "2024-03-13T15:01:23.045123456Z",
+              "computeAlternativeRoutes": false,
+              "routeModifiers": {
+                "avoidTolls": false,
+                "avoidHighways": false,
+                "avoidFerries": false
+              },
+              "languageCode": "en-US",
+              "units": "IMPERIAL"
             }
-          },
-          "destination":{
-            "location":{
-              "latLng":{
-                "latitude": 37.417670,
-                "longitude": -122.079595
-              }
-            }
-          },
-          "travelMode": "DRIVE",
-          "routingPreference": "TRAFFIC_AWARE",
-          "departureTime": "2023-10-15T15:01:23.045123456Z",
-          "computeAlternativeRoutes": false,
-          "routeModifiers": {
-            "avoidTolls": false,
-            "avoidHighways": false,
-            "avoidFerries": false
-          },
-          "languageCode": "en-US",
-          "units": "IMPERIAL"
-        }
-    """.trimIndent()
+        """.trimIndent()
 
+        val accessToken = ApiKey.accessToken
         val url = URL(urlString)
         val connection = url.openConnection() as HttpURLConnection
-        connection.requestMethod = "POST"
-        connection.setRequestProperty("Content-Type", "application/json")
-        connection.setRequestProperty("X-Goog-Api-Key", apiKey)
-        connection.setRequestProperty("Authorization","ya29.a0Ad52N39A7n8-kFoUB2mIHyFdburrSMqTJkYaWcSN8mmeWK204TM9t7AxUIXhIfflL89M6YEQyRylk8l7wevmHNDGeu_Xhm_w_uzx9tdt5uis7gIcmWdT39JbQpSQzlrMXWUjcl7I07sz_LLDx4Kx7VBbmxJ81YcHJuTVaCgYKAQwSARASFQHGX2MijYl6xf2kY0ye24B8cH-1Og0171")
-        connection.setRequestProperty("X-Goog-User-Project","surge-f7068")
-        connection.setRequestProperty(
-            "X-Goog-FieldMask",
-            "routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline"
-        )
-        connection.doOutput = true
+        try {
+            connection.requestMethod = "POST"
+            connection.setRequestProperty("Content-Type", "application/json")
+            connection.setRequestProperty("X-Goog-Api-Key", apiKey)
+            connection.setRequestProperty("Authorization", "Bearer $accessToken")
+            connection.setRequestProperty("X-Goog-User-Project", ApiKey.projectId)
+            connection.setRequestProperty(
+                "X-Goog-FieldMask",
+                "routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline"
+            )
+            connection.doOutput = true
 
-        val outputStreamWriter = OutputStreamWriter(connection.outputStream)
-        outputStreamWriter.write(jsonData)
-        outputStreamWriter.flush()
+            val outputStreamWriter = OutputStreamWriter(connection.outputStream)
+            outputStreamWriter.write(jsonData)
+            outputStreamWriter.flush()
 
-        val responseCode = connection.responseCode
-        println("Response Code: $responseCode")
+            val responseCode = connection.responseCode
+            Log.d("MyTag", "Response Code: $responseCode")
 
-        val response = connection.inputStream.bufferedReader().use { it.readText() }
-        Log.d("Response", response)
+            val response = if (responseCode == HttpURLConnection.HTTP_OK) {
+                connection.inputStream.bufferedReader().use { it.readText() }
+            } else {
+                connection.errorStream.bufferedReader().use { it.readText() }
+            }
+            Log.d("MyTag", "Response: $response")
 
-        connection.disconnect()
-
-        response.toString()
+            response
+        } catch (e: Exception) {
+            Log.e("MyTag", "Error: ${e.message}", e)
+            ""
+        }
     }
 }
